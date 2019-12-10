53ff27dcaf5fc5c79c3e72d8f0aeae7c1a986009
chris-bsnake <chris@battlesnake.com>
2019-12-10 08:41:22 -0800

    messing around

--- app/main.py
+++ app/main.py
@@ -5,14 +5,13 @@ import bottle
 
 from api import ping_response, start_response, move_response, end_response
 
-@bottle.route('/')
-def index():
-    return '''
-    Battlesnake documentation can be found at
-       <a href="https://docs.battlesnake.io">https://docs.battlesnake.io</a>.
-    '''
 
-@bottle.route('/static/<path:path>')
+@bottle.route("/")
+def index():
+    return ping_response()
+
+
+@bottle.route("/static/<path:path>")
 def static(path):
     """
     Given a path, return the static file located relative
@@ -20,9 +19,10 @@ def static(path):
 
     This can be used to return the snake head URL in an API response.
     """
-    return bottle.static_file(path, root='static/')
+    return bottle.static_file(path, root="static/")
 
-@bottle.post('/ping')
+
+@bottle.post("/ping")
 def ping():
     """
     A keep-alive endpoint used to prevent cloud application platforms,
@@ -30,7 +30,8 @@ def ping():
     """
     return ping_response()
 
-@bottle.post('/start')
+
+@bottle.post("/start")
 def start():
     data = bottle.request.json
 
@@ -46,7 +47,7 @@ def start():
     return start_response(color)
 
 
-@bottle.post('/move')
+@bottle.post("/move")
 def move():
     data = bottle.request.json
 
@@ -56,13 +57,13 @@ def move():
     """
     print(json.dumps(data))
 
-    directions = ['up', 'down', 'left', 'right']
+    directions = ["up", "down", "left", "right"]
     direction = random.choice(directions)
 
     return move_response(direction)
 
 
-@bottle.post('/end')
+@bottle.post("/end")
 def end():
     data = bottle.request.json
 
@@ -74,13 +75,14 @@ def end():
 
     return end_response()
 
+
 # Expose WSGI app (so gunicorn can find it)
 application = bottle.default_app()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     bottle.run(
         application,
-        host=os.getenv('IP', '0.0.0.0'),
-        port=os.getenv('PORT', '8080'),
-        debug=os.getenv('DEBUG', True)
+        host=os.getenv("IP", "0.0.0.0"),
+        port=os.getenv("PORT", "8080"),
+        debug=os.getenv("DEBUG", True),
     )
