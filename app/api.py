47aa938a5d56a259a2fb96ca1d1d80240dda605c
chris-bsnake <chris@battlesnake.com>
2019-12-11 09:58:04 -0800

    testing heads/tails svgs

--- app/api.py
+++ app/api.py
@@ -12,7 +12,7 @@ def start_response(color):
     return HTTPResponse(
         status=200,
         headers={"Content-Type": "application/json"},
-        body=json.dumps({"color": color, "headType": "rudolph", "tailType": "present"}),
+        body=json.dumps({"color": color, "headType": "bonhomme", "tailType": "flake"}),
     )
 
 
